83926c077832aad4ad3a64cf61fbc3e5b604dc18
haodo22 <haodo2285@gmail.com>
2026-08-06 16:03:44 +0700

    update hosting

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -107,7 +107,7 @@ export default function Home() {
 
           <div className="projects-grid">
             {projects.map(({ id, name, sourceImage, renderedImage, timestamp }) => (
-              <div className="project-card group">
+              <div key={id} className="project-card group">
                 <div className="preview">
                   <img
                     src={renderedImage || sourceImage || sourceImage}
